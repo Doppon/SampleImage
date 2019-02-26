7ecbaa8e66c2df475fe7618be904af8cae9a4ef3
Doppon <zaxpkuraudo7777@gmail.com>
2019-02-26 15:49:35 +0900

    bookモデルを追加

--- book/models.py
+++ book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+  book_title = models.CharField(max_length=30)
+  created_at = models.DateTimeField('登録日時', default=timezone.now)
