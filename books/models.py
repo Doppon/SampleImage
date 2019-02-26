275b8f7c1c247f4ddc7eec11c24eb5cafe563cbb
Doppon <zaxpkuraudo7777@gmail.com>
2019-02-26 16:38:43 +0900

    管理画面から見た際にわかりやすくするためタイトル名で返すように設定

--- books/models.py
+++ books/models.py
@@ -3,5 +3,9 @@ from django.utils import timezone
 
 
 class Book(models.Model):
-  book_title = models.CharField(max_length=30)
-  created_at = models.DateTimeField('登録日時', default=timezone.now)
+	title = models.CharField(max_length=30)
+	created_at = models.DateTimeField('登録日時', default=timezone.now)
+
+	def __str__(self):
+		return self.title
+	
