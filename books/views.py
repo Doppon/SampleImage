a928656bbd2fc8e86663d0fbac028b7a8da38e43
Doppon <zaxpkuraudo7777@gmail.com>
2019-02-26 17:18:39 +0900

    detailページとindexのテンプレートを追加

--- books/views.py
+++ books/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    book_list = Book.objects.order_by('-created_at')
+    context = {'book_list': book_list}
+    return render(request, 'index.html', context)
+
+def detail(request, book_id):
+    return HttpResponse("Hello, detail world.")
