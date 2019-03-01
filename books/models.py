67cb77a3237ed1534ce60da5b20b90ebc359eee9
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-01 18:01:50 +0900

    Imageフィールドの追加

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class Book(models.Model):
 	title = models.CharField(max_length=30)
+	logo = models.ImageField('ロゴ', upload_to='images/', blank=True)
 	created_at = models.DateTimeField('登録日時', default=timezone.now)
 
 	def __str__(self):
