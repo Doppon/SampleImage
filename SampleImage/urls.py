224468e252f2e2a517048222d5be7b6ec6907abd
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-14 21:42:00 +0900

    booksのルーティングを変更

--- SampleImage/urls.py
+++ SampleImage/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls')),
+    path('', include('books.urls')),
 ]
 
 # media
