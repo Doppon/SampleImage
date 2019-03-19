60e658cdd0a619fbf9aaacc5af4af0db5fc42b33
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-19 20:17:42 +0900

    Bookモデルにget_absolute_urlを設定

--- books/models.py
+++ books/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Book(models.Model):
-	title = models.CharField(max_length=30)
-	logo = models.ImageField('ロゴ', upload_to='images/', blank=True)
-	created_at = models.DateTimeField('登録日時', default=timezone.now)
+    title = models.CharField(max_length=30)
+    logo = models.ImageField('ロゴ', upload_to='images/', blank=True)
+    created_at = models.DateTimeField('登録日時', default=timezone.now)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("index")
 	
